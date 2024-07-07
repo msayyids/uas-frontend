ac1e5d3ede5b480c24ee0e7cf5986a3245a6b0a9
msayyids <muhammadsayyid525@gmail.com>
2024-07-08 00:04:48 +0700

    fix image region card

--- src/components/RegionCard/regionCard.jsx
+++ src/components/RegionCard/regionCard.jsx
@@ -5,6 +5,10 @@ import {
   NumberItem,
 } from "./regionCard.styled";
 
+import maskImage from "../../asset/icons8-protection-mask.png";
+import smileImage from "../../asset/icons8-smiling.png";
+import skullImage from "../../asset/icons8-skull.png";
+
 const RegionCard = ({ name, numbers }) => {
   return (
     <CardWrapper>
@@ -12,15 +16,15 @@ const RegionCard = ({ name, numbers }) => {
       <CardNumbers>
         <NumberItem status="confirmed" data-title="Confirmed">
           {numbers.confirmed.toLocaleString()}{" "}
-          <img  src="src/asset/icons8-protection-mask.png" alt="" />
+          <img  src={maskImage} alt="" />
         </NumberItem>
         <NumberItem status="recovered" data-title="Recovered">
           {numbers.recovered.toLocaleString()}{" "}
-          <img src="src/asset/icons8-smiling.png" alt="" />
+          <img src={smileImage} alt="" />
         </NumberItem>
         <NumberItem status="death" data-title="Deaths">
           {numbers.death.toLocaleString()}{" "}
-          <img src="src/asset/icons8-skull.png" alt="" />
+          <img src={skullImage} alt="" />
         </NumberItem>
       </CardNumbers>
     </CardWrapper>
